db7ec0738bd8221d6579763a2d6b983bbf5d9305
jain651 <jain651@usc.edu>
2021-02-05 10:16:01 -0700

    in process of scaling to 1:1 scale

---
+++ include/dirackernels/HoopReinforcement.h
@@ -0,0 +1,42 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// Moose Includes
+#include "DiracKernel.h"
+
+// Forward Declarations
+class HoopReinforcement;
+
+template <>
+InputParameters validParams<HoopReinforcement>();
+
+/**
+ * TOOD
+ */
+class HoopReinforcement : public DiracKernel
+{
+public:
+  static InputParameters validParams();
+
+  HoopReinforcement(const InputParameters & parameters);
+
+  virtual void addPoints() override;
+
+protected:
+  virtual Real computeQpResidual() override;
+
+  const Real & _eps;
+  const Real & _fy;
+  const Real & _E;
+  const Real & _A;
+  std::vector<Real> _point_param;
+  Point _p;
+};
---
+++ src/dirackernels/HoopReinforcement.C
@@ -0,0 +1,59 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HoopReinforcement.h"
+
+registerMooseObject("BlackBearApp", HoopReinforcement);
+
+defineLegacyParams(HoopReinforcement);
+
+InputParameters
+HoopReinforcement::validParams()
+{
+  InputParameters params = DiracKernel::validParams();
+  params.addRequiredParam<Real>("strain", "out of plane strain");
+  params.addRequiredParam<Real>("yield_strength", "Yield strength of the rebar");
+  params.addRequiredParam<Real>("elastic_modulus", "Elastic modulus of the rebar");
+  params.addRequiredParam<Real>("area", "Area of the rebar");
+  params.addRequiredParam<std::vector<Real>>("point", "The x,y,z coordinates of the point");
+  params.declareControllable("value");
+  return params;
+}
+
+HoopReinforcement::HoopReinforcement(const InputParameters & parameters)
+  : DiracKernel(parameters),
+    _eps(getParam<Real>("strain")),
+    _fy(getParam<Real>("yield_strength")),
+    _E(getParam<Real>("elastic_modulus")),
+    _A(getParam<Real>("area")),
+    _point_param(getParam<std::vector<Real>>("point"))
+{
+  _p(0) = _point_param[0];
+
+  if (_point_param.size() > 1)
+  {
+    _p(1) = _point_param[1];
+
+    if (_point_param.size() > 2)
+      _p(2) = _point_param[2];
+  }
+}
+
+void
+HoopReinforcement::addPoints()
+{
+  addPoint(_p);
+}
+
+Real
+HoopReinforcement::computeQpResidual()
+{
+  //  This is negative because it's a forcing function that has been brought over to the left side
+  return -_test[_i][_qp] * fmin(_E * _eps, _fy) * _A;
+}
