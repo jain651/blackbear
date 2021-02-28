67567cf9e4ea5042e18559c74167ca69adcb14ff
jain651 <jain651@usc.edu>
2021-02-28 11:39:01 -0700

    hoopreinf_running backing up for points in file commented code

--- src/dirackernels/HoopReinforcement.C
+++ src/dirackernels/HoopReinforcement.C
@@ -17,7 +17,7 @@ InputParameters
 HoopReinforcement::validParams()
 {
   InputParameters params = DiracKernel::validParams();
-  params.addRequiredParam<VariableName>("hoop_strain", "a component of strain tensor");
+  params.addRequiredParam<VariableName>("disp_component", "a component of strain tensor");
   params.addRequiredParam<Real>("yield_strength", "Yield strength of the rebar");
   params.addRequiredParam<Real>("youngs_modulus", "Elastic modulus of the rebar");
   params.addRequiredParam<Real>("area", "Area of the rebar");
@@ -30,13 +30,7 @@ HoopReinforcement::validParams()
 
 HoopReinforcement::HoopReinforcement(const InputParameters & parameters)
   : DiracKernel(parameters),
-    _number(_subproblem
-                    .getVariable(_tid,
-                                 parameters.get<VariableName>("hoop_strain"),
-                                 Moose::VarKindType::VAR_ANY,
-                                 Moose::VarFieldType::VAR_FIELD_STANDARD)
-                    .number()),
-    _system(_subproblem.getSystem(getParam<VariableName>("hoop_strain"))),
+    _system(_subproblem.getSystem(getParam<VariableName>("disp_component"))),
     _fy(getParam<Real>("yield_strength")),
     _E(getParam<Real>("youngs_modulus")),
     _A(getParam<Real>("area")),
@@ -71,7 +65,7 @@ HoopReinforcement::addPoints()
 Real
 HoopReinforcement::computeQpResidual()
 {
-  Real strain = _system.point_value(_number, _current_point, false);
+  Real strain = _system.point_value(_var.number(), _current_point, false)/_current_point(0);
   Real force;
   if(strain>0.)
     force = - fmin(_E*strain, _fy) * _A;
@@ -80,4 +74,18 @@ HoopReinforcement::computeQpResidual()
   // out <<" out force " << force << " pt "<< _current_point(0) << " " << _current_point(1) << " " << _current_point(2) << std::endl;
 
   return -force;
+}
+
+Real
+HoopReinforcement::computeQpJacobian()
+{
+  Real strain = _system.point_value(_var.number(), _current_point, false)/_current_point(0);
+  Real dstrain = 1./_current_point(0);
+  Real dforce;
+  if(strain>0.)
+    dforce = - fmin(_E*dstrain, _fy) * _A;
+  else
+    dforce = - fmax(_E*dstrain, _fy) * _A;
+
+  return -dforce;
 }
