acc9c11abb20ed00195ba2d6c0bddd0a8ffc84a5
jain651 <jain651@usc.edu>
2021-05-04 15:44:32 -0600

    waiting to resolve convergence issue with contact

--- include/actions/EqualValueEmbeddedConstraintAction.h
+++ include/actions/EqualValueEmbeddedConstraintAction.h
@@ -30,8 +30,6 @@ public:
 
 protected:
   std::vector<SubdomainName> _primary_block;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   std::vector<SubdomainName> _secondary_block;
 
@@ -46,28 +44,4 @@ protected:
 
   /// Penalty parameter used in constraint enforcement for kinematic and penalty formulations
   const Real _penalty;
-=======
-=======
->>>>>>> EqualValueEmbeddedConstraintAction_v2
-  std::vector<SubdomainName> _secondary_block;
-  /**
-   * The variable on the primary elem.
-   * @return MooseVariable & a reference to the primary variable
-   */
-  // virtual MooseVariable & primaryVariable() { return _primary_var; }
-  /// Vector of displacement variables
-  std::vector<NonlinearVariableName> _displacements;
-  std::vector<NonlinearVariableName> _primary_var;
-  // std::vector<MooseVariable> _primary_var1;
-  // std::vector<MooseVariable> _primary_var2;
-  /// Formulations, currently only supports KINEMATIC and PENALTY
-  const enum class Formulation { KINEMATIC, PENALTY } _formulation;
-  /// Penalty parameter used in constraint enforcement for kinematic and penalty formulations
-  const Real _penalty;
-  /// Formulations, currently only supports KINEMATIC and PENALTY
-  /// Penalty parameter used in constraint enforcement for kinematic and penalty formulations
-<<<<<<< HEAD
->>>>>>> EqualValueEmbeddedConstraintAction_v2
-=======
->>>>>>> EqualValueEmbeddedConstraintAction_v2
 };
